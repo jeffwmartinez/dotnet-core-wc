60e41baade9cd84724a7b4ed1ab3033097d8817c
Jeff Martinez <jeffxmartinez@gmail.com>
2020-02-25 14:40:42 -0800

    adding dacpac and sql db

---
+++ WinContainerCore/WinContainerCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinContainerCore.Models;
+
+namespace WinContainerCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+     : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Tasks { get; set; }
+
+    }
+}
--- WinContainerCore/WinContainerCore/Startup.cs
+++ WinContainerCore/WinContainerCore/Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WinContainerCore.Data;
 
 namespace WinContainerCore
 {
@@ -23,6 +25,9 @@ namespace WinContainerCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
